541d6ebfbb59a5bf654f5dba4e8dd0e13fe705d0
XM <xm@tqzgc.com>
2019-11-04 15:13:36 +0800

    :arrow_up: version 1.0.6

--- types/utils/hash/sha1.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Copy from https://github.com/h2non/jshashes/blob/master/hashes.js
- */
-declare const SHA1: (options?: any) => void;
-export default SHA1;
--- types/utils/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * remove ^\/+
- * @param name
- */
-export declare function objectName(name: string): string;
-export declare function escapeName(name: string): string;
-export declare function isHttpsProtocol(): boolean;
-export declare function setRegion(region: string, secure: boolean, internal?: boolean): string;
-export declare function typeOf(object: any): any;
